70b83d6c0e6f119e94e02cc7f73049e1e2f58280
Ilija Hadzic <ih2435@columbia.edu>
2025-04-07 22:40:52 -0400

    video_processor: publish detected corners image
    Primarily for debugging an visualization
    
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- vision/include/video_processor.hpp
+++ vision/include/video_processor.hpp
@@ -15,6 +15,7 @@ public:
 private:
     void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
     image_transport::Subscriber img_subscription_;
+    image_transport::Publisher img_goodfeature_pub_;
     rclcpp::Node::SharedPtr node_;
     int max_corners_;
 };
--- vision/src/video_processor.cpp
+++ vision/src/video_processor.cpp
@@ -10,6 +10,7 @@ VideoProcessor::VideoProcessor(rclcpp::Node::SharedPtr node) : max_corners_(200)
     image_transport::ImageTransport it(node_);
     img_subscription_ = it.subscribe("/camera/image_raw", 1,
                                      &VideoProcessor::imageCallback, this);
+    img_goodfeature_pub_ = it.advertise("goodfeature/image_raw", 5);
     RCLCPP_INFO(node_->get_logger(), "max corners is %d", max_corners_);
 }
 
@@ -33,9 +34,22 @@ void VideoProcessor::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr
 
         /// Apply corner detection
         cv::goodFeaturesToTrack(gray_frame, corners, max_corners_,
-                                quality_level, min_distance, cv::Mat(), block_size,
-                                use_harris_detector, k);
-        RCLCPP_INFO(node_->get_logger(), "number of corners detected: %ld", corners.size());
+                                quality_level, min_distance, cv::Mat(),
+                                block_size, use_harris_detector, k);
+        /// Draw corners detected
+        int r = 4;
+        for (const cv::Point2f& corner : corners) {
+            cv::circle(frame, corner, r, cv::Scalar(rng.uniform(0, 255),
+                                                    rng.uniform(0, 255),
+                                                    rng.uniform(0, 255)),
+                       -1, 8, 0);
+        }
+        RCLCPP_INFO(node_->get_logger(), "number of corners detected: %ld",
+                    corners.size());
+
+        sensor_msgs::msg::Image::SharedPtr goodfeature_msg =
+            cv_bridge::CvImage(msg->header, "bgr8", frame).toImageMsg();
+        img_goodfeature_pub_.publish(goodfeature_msg);
     }
     catch (cv_bridge::Exception& e)
     {
