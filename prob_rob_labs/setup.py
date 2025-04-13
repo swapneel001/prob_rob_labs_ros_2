5c028fde213a754bebf8a6756f817acae5cfcf77
Ilija Hadzic <ih2435@columbia.edu>
2025-04-13 16:43:37 -0400

    add prob_rob_labs package skeleton
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

---
+++ prob_rob_labs/setup.py
@@ -0,0 +1,34 @@
+from glob import glob
+import os
+
+from setuptools import find_packages, setup
+
+package_name = 'prob_rob_labs'
+
+launch_files = glob('launch/*_launch.py') + \
+    glob('launch/*_launch.xml') + \
+    glob('launch/*.yaml') + \
+    glob('launch/*.yml')
+
+data_files=[
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+    (os.path.join('share', package_name, 'launch'), launch_files),
+]
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(where='src', exclude=['test']),
+    package_dir={'': 'src'},
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ilija Hadzic',
+    maintainer_email='ih2435@columbia.edu',
+    description='Prob Rob Labs',
+    license='MIT',
+    tests_require=['pytest'],
+    # add entry points here
+)
