f25c6b41ea97c4abd91b0becf4d93477f22dbd04
Ilija Hadzic <ih2435@columbia.edu>
2025-04-16 23:49:19 -0400

    labs/image_mean_feature_x: implement mean detector
    The node takes the list of features, calculates the mean
    of x coordinates and publishes to /feature_mean topic.
    
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- prob_rob_labs/src/image_mean_feature_x/image_mean_feature_x.py
+++ prob_rob_labs/src/image_mean_feature_x/image_mean_feature_x.py
@@ -1,22 +1,27 @@
 import rclpy
+import numpy
 from rclpy.node import Node
-
-
-heartbeat_period = 1
+from std_msgs.msg import Float64
+from prob_rob_msgs.msg import Point2DArrayStamped
 
 class ImageMeanFeatureX(Node):
 
     def __init__(self):
         super().__init__('image_mean_feature_x')
         self.log = self.get_logger()
-        self.timer = self.create_timer(heartbeat_period, self.heartbeat)
-
-    def heartbeat(self):
-        self.log.info('heartbeat')
+        self.sub_features = self.create_subscription(
+            Point2DArrayStamped, '/goodfeature/corners',
+            self.handle_features, 1)
+        self.pub_mean = self.create_publisher(
+            Float64, '/feature_mean', 1)
 
     def spin(self):
         rclpy.spin(self)
 
+    def handle_features(self, f):
+        x_features = [ p.x for p in f.points ]
+        mean_x = numpy.mean(x_features)
+        self.pub_mean.publish(Float64(data=mean_x))
 
 def main():
     rclpy.init()
