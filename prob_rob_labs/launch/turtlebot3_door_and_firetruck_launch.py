51cea35a193f77ebb39fcc05f757e3a1d8ccf0f5
Ilija Hadzic <ih2435@columbia.edu>
2025-04-19 15:57:07 -0400

    labs/door+firetruck: expose run-flags to the top level
    make run_door_opener and run_vision_processor
    settable from the command line.
    
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- prob_rob_labs/launch/turtlebot3_door_and_firetruck_launch.py
+++ prob_rob_labs/launch/turtlebot3_door_and_firetruck_launch.py
@@ -4,20 +4,42 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     prob_rob_labs_dir = get_package_share_directory('prob_rob_labs')
+
+    run_vision_processor = LaunchConfiguration('run_vision_processor', default='false')
+    run_door_opener = LaunchConfiguration('run_door_opener', default='false')
+
+    declare_run_vision_processor_arg = DeclareLaunchArgument(
+        'run_vision_processor',
+        default_value='false',
+        description='Whether to run the vision processing nodes'
+    )
+
+    declare_run_door_opener_arg = DeclareLaunchArgument(
+        'run_door_opener',
+        default_value='false',
+        description='Whether to run the flaky door opener node'
+    )
 
     main_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(prob_rob_labs_dir, 'launch', 'turtlebot3_and_door_launch.py')
         ),
         launch_arguments={
-            'world': 'door_and_firetruck.world'
+            'world': 'door_and_firetruck.world',
+            'run_vision_processor': run_vision_processor,
+            'run_door_opener': run_door_opener
         }.items()
     )
 
-    return LaunchDescription([main_launch])
+    return LaunchDescription([
+        declare_run_vision_processor_arg,
+        declare_run_door_opener_arg,
+        main_launch
+    ])
