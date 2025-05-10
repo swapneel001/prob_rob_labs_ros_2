97e339b25f41669e5301f05782d761aa6eb5a1fb
Ilija Hadzic <ih2435@columbia.edu>
2025-05-10 15:55:32 -0400

    tools/template: add description next to the use_sim_time parameter
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- tools/template/template_launch.py
+++ tools/template/template_launch.py
@@ -8,7 +8,8 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='true'),
+        DeclareLaunchArgument('use_sim_time', default_value='true',
+                              description='set to true for simulation'),
         Node(
             package='prob_rob_labs',
             executable='NODE',
