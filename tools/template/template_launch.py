0279616c97368da9b734df7699895d83c276e205
Ilija Hadzic <ih2435@columbia.edu>
2025-05-09 23:04:10 -0400

    tools/template: add use_sim_time argument
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- tools/template/template_launch.py
+++ tools/template/template_launch.py
@@ -1,16 +1,18 @@
 import os
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    param_dic = {}
     return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
         Node(
             package='prob_rob_labs',
             executable='NODE',
-            name='NODE'
-
+            name='NODE',
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
         )
     ])
