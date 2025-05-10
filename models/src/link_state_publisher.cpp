4af863abc1876a81875e594e17d42706ed0e6cfe
Ilija Hadzic <ih2435@columbia.edu>
2025-05-10 18:00:46 -0400

    models: add link-state publisher plugin
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

---
+++ models/src/link_state_publisher.cpp
@@ -0,0 +1,73 @@
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/physics/physics.hh>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <gazebo_msgs/msg/link_states.hpp>
+
+namespace gazebo
+{
+class LinkStatePublisherPlugin : public WorldPlugin
+{
+public:
+  void Load(physics::WorldPtr _world, sdf::ElementPtr) override
+  {
+    world_ = _world;
+    node_ = rclcpp::Node::make_shared("link_state_publisher");
+    publisher_ = node_->create_publisher<gazebo_msgs::msg::LinkStates>("/gazebo/link_states", 10);
+
+    update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      std::bind(&LinkStatePublisherPlugin::OnUpdate, this));
+    
+    rclcpp_thread_ = std::thread([this]() {
+      rclcpp::spin(node_);
+    });
+
+    RCLCPP_INFO(node_->get_logger(), "LinkStatePublisherPlugin loaded.");
+  }
+
+  void OnUpdate()
+  {
+    gazebo_msgs::msg::LinkStates msg;
+
+    for (const auto& model : world_->Models())
+    {
+      for (const auto& link : model->GetLinks())
+      {
+        msg.name.push_back(link->GetScopedName());
+
+        ignition::math::Pose3d pose = link->WorldPose();
+        geometry_msgs::msg::Pose ros_pose;
+        ros_pose.position.x = pose.Pos().X();
+        ros_pose.position.y = pose.Pos().Y();
+        ros_pose.position.z = pose.Pos().Z();
+        ros_pose.orientation.x = pose.Rot().X();
+        ros_pose.orientation.y = pose.Rot().Y();
+        ros_pose.orientation.z = pose.Rot().Z();
+        ros_pose.orientation.w = pose.Rot().W();
+        msg.pose.push_back(ros_pose);
+      }
+    }
+
+    publisher_->publish(msg);
+  }
+
+  ~LinkStatePublisherPlugin() override
+  {
+    rclcpp::shutdown();
+    if (rclcpp_thread_.joinable())
+      rclcpp_thread_.join();
+  }
+
+private:
+  physics::WorldPtr world_;
+  event::ConnectionPtr update_connection_;
+
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<gazebo_msgs::msg::LinkStates>::SharedPtr publisher_;
+  std::thread rclcpp_thread_;
+};
+
+GZ_REGISTER_WORLD_PLUGIN(LinkStatePublisherPlugin)
+}
