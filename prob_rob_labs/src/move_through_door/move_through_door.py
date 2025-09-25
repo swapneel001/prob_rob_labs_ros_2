463074450a8f5facd685873ded23875b348fca84
swapneel_probroblab <sdb2183@columbia.edu>
2025-09-25 19:58:39 -0400

    Lab2: A3.2 done

--- prob_rob_labs/src/move_through_door/move_through_door.py
+++ prob_rob_labs/src/move_through_door/move_through_door.py
@@ -2,7 +2,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 from std_msgs.msg import Empty
-max_torque = 5.0
+from geometry_msgs.msg import Twist
+
 torque_hold = 10
 heartbeat_period = 0.1
 
@@ -12,12 +13,22 @@ class MoveThroughDoor(Node):
         super().__init__('move_through_door')
         self.log = self.get_logger()
         self.pub_torque = self.create_publisher(Float64,'/hinged_glass_door/torque',1)
+        self.pub_vel = self.create_publisher(Twist,'/cmd_vel',10)
         self.timer = self.create_timer(heartbeat_period, self.heartbeat)
         self.torque = 5.0
+        self.heartbeat_counter = 0
+        
     def heartbeat(self):
         self.log.info('heartbeat')
         self.pub_torque.publish(Float64(data =self.torque)) 
+        self.heartbeat_counter+=1
+        if self.heartbeat_counter>50:
+            self.move_robot()
 
+    def move_robot(self):
+        vel_msg = Twist()
+        vel_msg.linear.x = 2.0
+        self.pub_vel.publish(vel_msg)
     def spin(self):
         rclpy.spin(self)
 
