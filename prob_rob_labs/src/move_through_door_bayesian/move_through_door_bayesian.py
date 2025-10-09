3dc2a3f5b4247cc76de51c5116ee8d49848c49db
swapneel_kde <sdb2183@columbia.edu>
2025-10-09 10:50:45 -0400

    Lab3:A5 Updated Probabilities

--- prob_rob_labs/src/move_through_door_bayesian/move_through_door_bayesian.py
+++ prob_rob_labs/src/move_through_door_bayesian/move_through_door_bayesian.py
@@ -3,18 +3,21 @@ import time
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 
 heartbeat_period = 0.1
 
 '''
 Results obtained for conditional probabilities:
-Threshold=235.0, trials=5, samples_per_trial=1000
+3 trials,1000 samples per trial, threshold=235.0
+Resetting world between trials.
 
-P(z=open|x=open) = 0.892 
-P(z=closed|x=open) = 0.108
 
-P(z=open|x=closed) = 0.018
-P(z=closed|x=closed) = 0.982
+P(z=open|x=open) = 0.645
+P(z=closed|x=open) = 0.355
+
+P(z=open|x=closed) = 0.002
+P(z=closed|x=closed) = 0.998
 '''
 
 
@@ -31,10 +34,10 @@ class MoveThroughDoorBayesian(Node):
         self.sub_feature_mean = self.create_subscription(Float64, '/feature_mean', self.check_feature_mean, 10)
 
         self.torque = 5.0
-        self.cmd_phase_sec = 3.0
-        self.settle_sec = 2.0
+        self.cmd_phase_sec = 5.0
+        self.settle_sec = 5.0
 
-        self.trials_total = 5
+        self.trials_total = 3
         self.samples_per_trial = 1000
         self.trial_idx = 0
 
@@ -53,8 +56,19 @@ class MoveThroughDoorBayesian(Node):
         self.zopen_xclosed = 0
         self.zclosed_xclosed = 0
 
+        self.reset_client = self.create_client(Empty, '/reset_world')
+        while not self.reset_client.wait_for_service(timeout_sec=1.0):
+            self.log.info('Waiting for /reset_world service...')
+
         self.timer = self.create_timer(heartbeat_period, self.heartbeat)
         self.log.info(f'Threshold={self.threshold}, trials={self.trials_total}, samples_per_trial={self.samples_per_trial}')
+
+    def reset_world(self):
+        req = Empty.Request()
+        self.reset_client.call_async(req)
+        self.log.info('Resetting world...')
+        time.sleep(1.0)  # wait briefly for reset to take effect
+        self.log.info('World reset done.')
 
     def now(self):
         return time.time()
@@ -65,13 +79,19 @@ class MoveThroughDoorBayesian(Node):
             self.state_start = t
         dt = t - self.state_start
 
+        # Helper for concise logging
+        def stage(msg):
+            self.log.info(f'[Trial {self.trial_idx+1}/{self.trials_total}] Stage: {msg}')
+
         if self.state == 'close_cmd':
+            stage('CLOSE_CMD (closing door)')
             self.pub_torque.publish(Float64(data=-self.torque))
             if dt >= self.cmd_phase_sec:
                 self.state = 'close_settle'
                 self.state_start = t
 
         elif self.state == 'close_settle':
+            stage('CLOSE_SETTLE (holding door closed)')
             self.pub_torque.publish(Float64(data=-self.torque))
             if dt >= self.settle_sec:
                 self.state = 'close_measure'
@@ -79,21 +99,26 @@ class MoveThroughDoorBayesian(Node):
                 self.measure_active = True
                 self.current_x_open = False
                 self.measure_count = 0
+                self.log.info(f'[Trial {self.trial_idx+1}] Measuring CLOSED state...')
 
         elif self.state == 'close_measure':
+            stage(f'CLOSE_MEASURE ({self.measure_count}/{self.samples_per_trial})')
             self.pub_torque.publish(Float64(data=-self.torque))
             if self.measure_count >= self.samples_per_trial:
                 self.measure_active = False
                 self.state = 'open_cmd'
                 self.state_start = t
+                self.log.info(f'[Trial {self.trial_idx+1}] Finished CLOSED measurement.')
 
         elif self.state == 'open_cmd':
+            stage('OPEN_CMD (opening door)')
             self.pub_torque.publish(Float64(data=self.torque))
             if dt >= self.cmd_phase_sec:
                 self.state = 'open_settle'
                 self.state_start = t
 
         elif self.state == 'open_settle':
+            stage('OPEN_SETTLE (holding door open)')
             self.pub_torque.publish(Float64(data=self.torque))
             if dt >= self.settle_sec:
                 self.state = 'open_measure'
@@ -101,15 +126,20 @@ class MoveThroughDoorBayesian(Node):
                 self.measure_active = True
                 self.current_x_open = True
                 self.measure_count = 0
+                self.log.info(f'[Trial {self.trial_idx+1}] Measuring OPEN state...')
 
         elif self.state == 'open_measure':
+            stage(f'OPEN_MEASURE ({self.measure_count}/{self.samples_per_trial})')
             self.pub_torque.publish(Float64(data=self.torque))
             if self.measure_count >= self.samples_per_trial:
                 self.measure_active = False
                 self.trial_idx += 1
+                self.log.info(f'[Trial {self.trial_idx}] Completed full open/close cycle.')
                 if self.trial_idx < self.trials_total:
                     self.state = 'close_cmd'
                     self.state_start = t
+                    self.reset_world()   # reset between trials
+                    self.log.info(f'--- Starting Trial {self.trial_idx+1}/{self.trials_total} ---')
                 else:
                     self.report_results()
                     rclpy.shutdown()
