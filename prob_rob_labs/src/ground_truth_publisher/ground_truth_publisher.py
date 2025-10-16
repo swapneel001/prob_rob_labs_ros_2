edbf688eb648268098ae82d65faa711874f3f63f
swapneel_kde <sdb2183@columbia.edu>
2025-10-16 18:46:30 -0400

    lab 4 a1 almost done

--- prob_rob_labs/src/ground_truth_publisher/ground_truth_publisher.py
+++ prob_rob_labs/src/ground_truth_publisher/ground_truth_publisher.py
@@ -16,21 +16,40 @@ class GroundTruthPublisher(Node):
         self.pose_publisher = self.create_publisher(PoseStamped, '/tb3/ground_truth/pose', 10)
         self.twist_publisher = self.create_publisher(TwistStamped, '/tb3/ground_truth/twist', 10)
         self.waffle_pi_footprint = 'waffle_pi::base_footprint' 
+        #self.declare_parameter('reference_frame','odom')
 
-
+    def heartbeat(self):
+        pass
     def link_states_callback(self, msg):
         try:
             idx = msg.name.index(self.waffle_pi_footprint)
             self.current_pose = msg.pose[idx]
             self.current_twist = msg.twist[idx]
+            #self.log.info(f"Current Pose: {self.current_pose}")
+            self.publish_ground_truth(msg.pose[idx],msg.twist[idx])
+
         except ValueError:
             self.log.error(f"Link '{self.waffle_pi_footprint}' not found in /gazebo/link_states")
             self.current_pose = None
             self.current_twist = None
-        
-        robot_pose = msg.pose[idx]
-        robot_twist = msg.twist[idx]
-        
+    
+    def publish_ground_truth(self, pose,twist):
+            robot_pose = pose
+            robot_twist = twist
+            #frame = self.get_parameter('reference_frame').get_parameter_value().string_value
+            frame = 'odom'
+            time_stamp = self.get_clock().now().to_msg()
+            pose_msg = PoseStamped()
+            pose_msg.header.stamp = time_stamp
+            pose_msg.header.frame_id = frame
+            pose_msg.pose = robot_pose
+            self.pose_publisher.publish(pose_msg)
+            twist_msg = TwistStamped()
+            twist_msg.header.stamp = time_stamp
+            twist_msg.header.frame_id = frame
+            twist_msg.twist = robot_twist
+            self.twist_publisher.publish(twist_msg)
+
     def spin(self):
         rclpy.spin(self)
 
