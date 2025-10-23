1876de9ffe4f424297a65fcfc782ea9081976499
swapneel_kde <sdb2183@columbia.edu>
2025-10-23 19:06:33 -0400

    added subscribers for camera info and landmark points

--- prob_rob_labs/src/landmark_positioner/landmark_positioner.py
+++ prob_rob_labs/src/landmark_positioner/landmark_positioner.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from prob_rob_msgs.msg import Point2DArrayStamped
+from sensor_msgs.msg import CameraInfo
 
 
 heartbeat_period = 0.1
@@ -10,6 +12,21 @@ class LandmarkPositioner(Node):
         super().__init__('landmark_positioner')
         self.log = self.get_logger()
         self.timer = self.create_timer(heartbeat_period, self.heartbeat)
+        self.declare_parameter('landmark_color', 'cyan')
+        self.landmark_color = self.get_parameter('landmark_color').get_parameter_value().string_value
+        self.landmark_points_sub = self.create_subscription(
+            Point2DArrayStamped,
+            f'/vision_{self.landmark_color}/corners',
+            self.landmark_positioning_callback,
+            10
+        )
+
+        self.camera_info_sub = self.create_subscription(
+            CameraInfo,
+            '/camera/camera_info',
+            self.camera_info_callback,
+            10
+        )
 
     def heartbeat(self):
         self.log.info('heartbeat')
