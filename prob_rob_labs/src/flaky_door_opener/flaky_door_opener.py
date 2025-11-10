a2f9df4a623d284de4c6726633b888f9f7d5cfaa
Ilija Hadzic <ih2435@columbia.edu>
2025-11-09 21:13:42 -0500

    labs: more fixes to flaky door opener
    when in "push" state and the timer expires, the
    next callback would only transition back to idle
    state, which is wrong. If the timer has expired
    regardless of the state, the callback should
    select the new torque and evaluate whether to open
    the door again
    
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- prob_rob_labs/src/flaky_door_opener/flaky_door_opener.py
+++ prob_rob_labs/src/flaky_door_opener/flaky_door_opener.py
@@ -7,6 +7,9 @@ from std_msgs.msg import Empty
 max_torque = 5.0
 required_open_time = 3
 
+def to_sec(t):
+    return t.nanoseconds * 1e-9
+
 class FlakyDoorOpener(Node):
 
     def __init__(self):
@@ -16,22 +19,24 @@ class FlakyDoorOpener(Node):
             Float64, '/hinged_glass_door/torque', 1)
         self.sub_command = self.create_subscription(
             Empty, '/door_open', self.handle_command, 1)
-        self.state = 'trying push'
+        self.state = 'idle'
 
     def handle_command(self, _):
-        if self.state == 'trying push':
-            self.log.info(f'state: {self.state}')
+        def elapsed_time():
+            return to_sec(self.get_clock().now()) - self.push_start_time
+        self.log.info(f'state: {self.state}')
+        if self.state == 'idle' or \
+           self.state == 'pushing' and elapsed_time() > required_open_time:
             self.torque = random.choice([1.0, 0.0, 0.0, 0.0, 0.0]) * max_torque
             if self.torque != 0.0:
                 self.state = 'pushing'
-                self.push_start_time = self.get_clock().now()
-        elif self.state == 'pushing':
-            self.log.info(f'state: {self.state}')
-            elapsed_time = self.get_clock().now() - self.push_start_time
-            if elapsed_time.nanoseconds / 1e9 > required_open_time:
-                self.state = 'trying push'
-        self.log.info(f'door open requested using torque {self.torque}')
-        self.pub_torque.publish(Float64(data=self.torque))
+                self.push_start_time = to_sec(self.get_clock().now())
+                self.pub_torque.publish(Float64(data=self.torque))
+            else:
+                self.state = 'idle'
+            self.log.info(f'door open requested using torque {self.torque}')
+        else:
+            self.log.info('request while pushing dropped')
 
     def spin(self):
         rclpy.spin(self)
